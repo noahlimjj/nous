2c54936f14386b48a0276ba5bd4fa34f7bfd5c08
Noah Lim <User@Noah-MacBook-Air.local>
2026-01-27 00:01:39 +0800

    Fix duplicate appId declaration causing SyntaxError

--- index.js
+++ index.js
@@ -5435,8 +5435,6 @@ const Goals = ({ db, userId, setNotification }) => {
         );
     }
 
-    const appId = typeof __app_id !== 'undefined' ? __app_id : 'study-tracker-app';
-
     return React.createElement('div', { className: "max-w-4xl mx-auto p-4 sm:p-6 lg:p-8" },
         // Notebook section
         React.createElement(Notebook, { db, userId, setNotification }),
